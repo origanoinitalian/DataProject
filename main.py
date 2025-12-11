832ce03c9fb4eb2b73101e65e6dda97a4fbe8d95
origanoinitalian <avishanabdinejad@gmail.com>
2025-12-11 14:43:18 +0100

    feat:make dashboard with first histogram

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import dash_ag_grid as dag
 from src.utils.data_processor import DataProcessor, manage_data
 from src.utils.arg_parser import parse_arguments
 import pandas as pd
+from src.components.visualizations import create_magnitude_distribution_hist
 
 app = Dash(__name__)
 
@@ -14,6 +15,21 @@ def main():
         start_time=args.start_time,
         end_time=args.end_time
     )
+
+    try:
+        df = pd.read_csv('data/cleaned/cleaned_earthquakes.csv')
+
+    except FileNotFoundError:
+        print("Cleaned data file not found. Run with 'python3 main.py --fetch-data'.")
+        return
+
+    app.layout = html.Div([
+        html.H1("Earthquake Dashboard"),
+    
+        create_magnitude_distribution_hist(df)
+    ])
+
+    app.run(debug=True)
 
 if __name__ == "__main__":
 
