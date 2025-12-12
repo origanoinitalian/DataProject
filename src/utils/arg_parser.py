2d0c9f1193833c4938c5b879a93e508a8e70dbdc
origanoinitalian <avishanabdinejad@gmail.com>
2025-12-12 11:18:29 +0100

    feat:add start and end time to the parser arguments

--- src/utils/arg_parser.py
+++ src/utils/arg_parser.py
@@ -5,6 +5,20 @@ def parse_arguments() -> argparse.Namespace:
     parser.add_argument(
             '--fetch-data',
             action='store_true',
-            help='Download datas from the USGS API before starting the dashboard.'
+            help='Download datas from the USGS API before starting the dashboard.',
+        )
+    
+    parser.add_argument(
+            '--start-time',
+            type=str,
+            default=None,
+            help='Start date in YYYY-MM-DD format.',
+        )
+
+    parser.add_argument(
+            '--end-time',
+            type=str,
+            default=None,
+            help='End date in YYYY-MM-DD format.',
         )
     return parser.parse_args()
